878d60e4a391ccefea0b1cd19387a3fad8beeaf3
Your Name <you@example.com>
2026-02-18 16:32:42 +0300

    feat: Migrate server database from SQLite to PostgreSQL, adding a dedicated `db` service and updating configurations.

--- server/database.py
+++ server/database.py
@@ -7,7 +7,7 @@ import os
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./messenger.db")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
